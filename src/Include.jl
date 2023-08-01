bbfc758d2a634dc0376a40fd68859577c03f32d7
Jeffrey Varner <jdv27@cornell.edu>
2023-08-01 15:46:24 -0400

    Update Include.jl
    removed hello

--- src/Include.jl
+++ src/Include.jl
@@ -5,7 +5,6 @@ const _PATH_TO_SRC = dirname(pathof(@__MODULE__));
 # fill me in
 
 # load my codes -
-include(joinpath(_PATH_TO_SRC, "Hello.jl"));
 include(joinpath(_PATH_TO_SRC, "Types.jl"));
 include(joinpath(_PATH_TO_SRC, "Factory.jl"));
 include(joinpath(_PATH_TO_SRC, "Compute.jl"));
