7184cdaedd5594f9cf54c1602aac95dabc72b496
Jeffrey Varner <jdv27@cornell.edu>
2023-10-01 11:04:56 -0400

    Update Include.jl
    this should work

--- src/Include.jl
+++ src/Include.jl
@@ -10,6 +10,9 @@ using GLPK
 using LinearAlgebra
 using GridInterpolations
 
+# we need this for the cartesian product -
+import Base.Iterators: product
+
 # load my codes -
 include(joinpath(_PATH_TO_SRC, "Types.jl"));
 include(joinpath(_PATH_TO_SRC, "Factory.jl"));
